3721fdcf3dd33a25f9e18145dbcf7904dc4816bf
salaboy <salaboy@gmail.com>
2019-03-19 18:43:13 -0300

    Initial import

---
+++ src/main/java/com/salaboy/myservice2/MyService2Application.java
@@ -0,0 +1,13 @@
+package com.salaboy.myservice2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyService2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyService2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/salaboy/myservice2/MyService2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.salaboy.myservice2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyService2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
