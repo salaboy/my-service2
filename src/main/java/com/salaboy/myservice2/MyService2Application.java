73b268fbb8067009516f69b6af50d89a434acaea
salaboy <salaboy@gmail.com>
2019-03-19 18:51:31 -0300

    adding endpoint

--- src/main/java/com/salaboy/myservice2/MyService2Application.java
+++ src/main/java/com/salaboy/myservice2/MyService2Application.java
@@ -2,12 +2,19 @@ package com.salaboy.myservice2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MyService2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyService2Application.class, args);
 	}
 
+	@GetMapping
+	public String hello(){
+		return "hello from k8s";
+	}
 }
